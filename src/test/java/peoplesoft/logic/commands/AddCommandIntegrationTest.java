617072d23018338a3b8926859fb7c6bf231bd68c
zhongfu <me@zhongfu.li>
2022-03-28 16:59:08 +0800

    Fix edge case in AddCommandIntegrationTest
    seems like it happens when multiple Person-related tests are running in
    parallel?

--- src/test/java/peoplesoft/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/peoplesoft/logic/commands/AddCommandIntegrationTest.java
@@ -27,7 +27,7 @@ public class AddCommandIntegrationTest {
 
     @Test
     public void execute_newPerson_success() {
-        Person validPerson = new PersonBuilder().build();
+        Person validPerson = new PersonBuilder().withNextId().build();
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.addPerson(validPerson);
